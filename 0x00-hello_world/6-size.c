87b7927d735b4c71fb588db27f838a08dac7dfa9
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-04 16:47:00 -0700

    added ne line

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,15 @@ int main(void)
 	float d;
 	char c;
 
-	printf("Size of char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", sizeof(i));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
-	printf("Size of a float: %lu byte(s)\n", sizeof(d));
+	printf("Size of char: %lu
+		       	byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu
+		       	byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu
+		       	byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu
+		       	byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu
+		       	byte(s)\n", sizeof(d));
 	return (0);
 }
