50ee54f081e177740b59d0ae1e2a7db7b00c4568
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-04 16:43:19 -0700

    added file and lines

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: main function
+ */
+int main(void)
+{
+	int i;
+	long int l;
+	long long int ll;
+	float d;
+	char c;
+
+	printf("Size of char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+	printf("Size of a float: %lu byte(s)\n", sizeof(d));
+	return (0);
+}
