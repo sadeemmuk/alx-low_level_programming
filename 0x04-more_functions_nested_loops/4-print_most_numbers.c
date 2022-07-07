5305a93c7a817af5dec2b2a5fe4dbd6b0e329ee3
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-07 16:32:15 -0700

    added line

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers -> this print most numbers
+ */
+void print_most_numbers(void)
+{
+	int X;
+
+	for ( X = 0; X < 10; X++)
+        {
+             if(x != 2 && X != 4)
+		_putchar(X + '0');
+        }
+	_putchar('\n');
+}
