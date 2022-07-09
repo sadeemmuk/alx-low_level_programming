3133428f28b22fbd525c9b1228fa6b02746fe55c
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-09 11:07:07 -0700

    remove whitespace

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper-> this is a function to check upper case alphas 
+ * _isupper-> this is a function to check upper case alphas
  * @c: is an integer paramer
- * Return: something but success 
+ * Return: something but success
  */
 int _isupper(int c)
 {
