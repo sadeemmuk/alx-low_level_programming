ef10b3885167c801deff3371c56e6001b946c59a
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-08 18:26:33 -0700

    added file

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if integer is positive or negative
+ * Return
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive or negatine(i);
+
+	return (0);
+}
