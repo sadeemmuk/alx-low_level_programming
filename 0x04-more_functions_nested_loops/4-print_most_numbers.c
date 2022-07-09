88c4a6495dfc05664b4c095291e2709e9a60b479
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-09 11:50:33 -0700

    added line

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_most_numbers -> this print most numbers
+ * print_most_numbers - this print most numbers
+ * Return: 0
  */
 void print_most_numbers(void)
 {
 	int X;
 
-	for ( X = 0; X < 10; X++)
-        {
-             if(x != 2 && X != 4)
+	for (X = 0; X < 10; X++)
+	{
+	if (x != 2 && X != 4)
 		_putchar(X + '0');
-        }
+	}
 	_putchar('\n');
 }
