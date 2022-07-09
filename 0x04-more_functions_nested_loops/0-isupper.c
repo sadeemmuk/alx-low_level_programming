4a4d483b5ed49245aec3c83c9ecf2b7ac57dd9e8
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-09 05:29:04 -0700

    added line

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper -> this is a function to check upper case alphas 
- * @c: an integer paramer
+ * _isupper-> this is a function to check upper case alphas 
+ * @c: is an integer paramer
  * Return: something but success 
  */
 int _isupper(int c)
