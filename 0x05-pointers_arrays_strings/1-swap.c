45fdae742d06d2ab16c48b07c8ef96a241773cdc
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-12 13:49:29 -0700

    creat new line

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * swap_int -  a function that swaps the values of two integers
+ * @a: parameter 1
+ *@b: parmater 2
+ */
+void swap_int(int *a, int *b)
+{
+	int tap;
+
+	tap = *a;
+	*a = *b;
+	*b = tmp;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,5 @@
 #define MAIN.H
 int putchar(char c);
 int void reset_to_98(int *n);
-
+void swap_int(int *a, int *b
 #endif
