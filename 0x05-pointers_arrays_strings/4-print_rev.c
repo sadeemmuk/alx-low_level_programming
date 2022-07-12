c21233fd72623026dde0c0903256bb8b6032a859
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-12 16:14:18 -0700

    added lines

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,10 @@ void print_rev(char *s)
 {
 	int n;
 
-	for (n = 0; s[n] != '\0'; n++);
-	
-	for (n = n - 1; s[n] != '\0'; n--)
+	for (n = 0; s[n] != '\0'; n++)
+		;
+
+	for (n = n - 1; s[n] != '\0' ; n--)
 	{
 		_putchar(s[n]);
 	}
