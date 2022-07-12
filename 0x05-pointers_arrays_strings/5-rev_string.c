1e45ad7a86e6577654cb1510277006175b9eb7c4
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-12 16:34:01 -0700

    add

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - reverse the string
+ * @s: pointer to string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, j, k, tar;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		tar = s[k];
+		s[k] = s[j];
+		s[j] = tar;
+		k++;
+		j--;
+	}
+{
