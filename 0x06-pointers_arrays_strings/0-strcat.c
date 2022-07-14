8ceca8c32cc76dca1a3470e3f97618dbf5d87cfe
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-14 15:30:01 -0700

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,16 @@
  * @src: second
  * Return: a string
  */
+
 char *_strcat(char *dest, char *src)
 {
+	int index = 0, i = 0;
+
+	while (dest[index++])
+		i++;
+
+	for (index = 0; src[index]; i++)
+		dest[i++] = src[index];
+
 	return (dest);
 }
