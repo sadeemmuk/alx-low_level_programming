5fa45425bdef15d5565d0251dbfcbcbab95a3b0b
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-14 14:58:30 -0700

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
+#include "main.h"
 #include <stdio.h>
 #include <string.h>
-#include "main.h"
 
 /**
  * _strcat - this is a function strcat
@@ -10,16 +10,5 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
-
-	while (dest[len])
-		len++;
-
-	for (i = 0; src[i] != 0; i++)
-	{
-		dest[len] = src[i];
-		len += 1;
-	}
-	dest[len] = '\0';
 	return (dest);
 }
