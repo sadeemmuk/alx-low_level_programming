19dc27b61e3131eea0d6af3638dd2f2de254c93d
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-15 09:01:12 -0700

    added lines

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio>
 #include <string.h>
 
 /**
@@ -7,10 +6,17 @@
  * @src: first param
  * @dest: second param
  * @n: third param
- * Return: alwyas 0
+ * Return: always 0
  */
 char *_strncat(char *dest, char *src, int n);
 {
-	strncat(dest, src, n);
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
 	return (dest);
 }
