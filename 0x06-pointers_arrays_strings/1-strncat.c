f9b0b1cfac793a259a44c99a77a187e0a76fa0e2
sadeemmuk <sadeemmuk2012@gmail.com>
2022-07-15 09:14:02 -0700

    fixed

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <string.h>
-
 /**
  * _strncat - function to append new char
  * @src: first param
  * @dest: second param
  * @n: third param
- * Return: always 0
+ * Return: string
  */
+
 char *_strncat(char *dest, char *src, int n);
 {
 	int index = 0, dest_len = 0;
